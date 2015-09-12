665eb3979cb8b46f91de3ec88f79b34188e2c4e4
Rui Tang <tangrui.cn@gmail.com>
2015-09-12 16:19:38 +0800

    返回匿名函数.

--- src/redux/create-store.js
+++ src/redux/create-store.js
@@ -1,7 +1,7 @@
 import {applyMiddleware, createStore, combineReducers} from 'redux'
 import promiseMiddleware from 'redux-promise-middleware'
 
-export default function createReduxStore(data) {
+export default (data) => {
   let finalCreateStore = applyMiddleware(promiseMiddleware)(createStore)
   const reducers = require('./ducks')
   const store = finalCreateStore(combineReducers(reducers), data)
